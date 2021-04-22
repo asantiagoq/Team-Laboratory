78e9fce2208c8c511e98fad5f15e38cc00ad2718
Samir Diego Chávez Cáceres <extermilel@gmail.com>
2021-04-22 18:15:51 -0500

    Agregando las funciones de add y sub

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,17 @@
 
 public class Calculator {
-	int add(int a, int b){ return 0; }
-	  int sub(int a, int b){ return 0; }
-	  int mul(int a, int b){ return 0; }
-	  int div(int a, int b){ return 0; }
-	  int mod(int a, int b){ return 0; }
+
+	int add(int a, int b){ 
+		return a + b; 
+	}
+	
+	int sub(int a, int b){ 
+		return a - b; 
+	}
+
+	int mul(int a, int b){ return 0; }
+	int div(int a, int b){ return 0; }
+	int mod(int a, int b){ return 0; }
 }
 
 
